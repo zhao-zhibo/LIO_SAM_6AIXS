83618eaff9db005bed904e6f0b6998c9577e81d7
赵智博 <zbzhao@whu.edu.cn>
2025-02-19 21:23:02 +0800

    新增第三方库smallGicp，同时增加roadSide.cpp，专门处理路侧和车载匹配信息

---
+++ LIO-SAM-6AXIS/src/roadSide.cpp
@@ -0,0 +1,58 @@
+#include "utility.h"
+#include "lio_sam_6axis/cloud_info.h"
+#include <benchmark/read_points.hpp>
+#include <ann/kdtree_omp.hpp>
+#include <points/point_cloud.hpp>
+#include <factors/gicp_factor.hpp>
+#include <factors/plane_icp_factor.hpp>
+#include <util/downsampling_omp.hpp>
+#include <util/normal_estimation_omp.hpp>
+#include <registration/reduction_omp.hpp>
+#include <registration/registration.hpp>
+
+struct VelodynePointXYZIRT {
+    PCL_ADD_POINT4D
+
+    PCL_ADD_INTENSITY;
+    uint16_t ring;
+    float time;
+
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+POINT_CLOUD_REGISTER_POINT_STRUCT (VelodynePointXYZIRT,
+                                   (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)
+                                           (uint16_t, ring, ring)(float, time, time)
+)
+struct Velodyne_M1600PointXYZIRT {
+   PCL_ADD_POINT4D;
+  uint8_t intensity;
+  uint8_t ring;
+  uint32_t timestampSec;
+  uint32_t timestampNsec;
+  
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+    Velodyne_M1600PointXYZIRT,
+    (float, x, x)(float, y, y)(float, z, z)(uint8_t, intensity, intensity)(
+        uint8_t, ring, ring)(uint32_t, timestampSec, timestampSec)(uint32_t, timestampNsec, timestampNsec))
+
+// Use the Velodyne point format as a common representation
+using PointXYZIRT = VelodynePointXYZIRT;
+class RoadSide : public ParamServer {
+    ros::Subscriber subRoadSideCloud;
+};
+
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "lio_sam_6axis");
+
+    RoadSide RS;
+
+    ROS_INFO("\033[1;32m----> RoadSide LiDAR Started.\033[0m");
+
+    ros::MultiThreadedSpinner spinner(3);
+    spinner.spin();
+
+    return 0;
+}
